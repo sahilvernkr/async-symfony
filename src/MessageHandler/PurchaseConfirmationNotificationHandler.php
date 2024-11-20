4a4759858173cd79467aa06cab19ea2332733059
sahil <sahilvernekar96@gmail.com>
2024-11-20 09:01:29 +0530

    Create PurchaseConfirmationNotificationHandler

---
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class PurchaseConfirmationNotificationHandler
+{
+    public function __invoke(PurchaseConfirmationNotification $notification)
+    {
+        // 1. create a PDF contract note
+        echo "creating a PDF contract note...<br>";
+        
+        // 2. Email the contract note to the buyer 
+        echo "Emailing contract note to" . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+    }
+}
